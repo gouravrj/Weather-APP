13de2dd40270c2e926bc4dab919fc53d19abf669
Gourab Raj <89293518+gouravrj@users.noreply.github.com>
2022-11-13 23:56:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,21 @@
-let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
-getWeatherData = (city) => {
-  const URL = "https://api.openweathermap.org/data/2.5/weather";
-  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=celcius`;
-  const weatherPromise  = fetch(FULL_URL);
-  
-  return weatherPromise.then((response) => {
-    console.log(response);
-    return response.json();
-  })
-}
-searchCity = () => {
-  const city = document.getElementById('city-input').value;
-  getWeatherData(city)
-  .then((res)=>{
-    showWeatherData(res);
-    console.log(res);
-  }).catch((error)=>{
-    console.log(error);
-    console.log("Something happend");
-  })
-}
 
-showWeatherData = (weatherData) => {
-  document.getElementById("city-name").innerText = weatherData.name;
-  document.getElementById("weather-type").innerText = weatherData.weather[0].main;
-  document.getElementById("temp").innerText = weatherData.main.temp;
-  document.getElementById("min-temp").innerText = weatherData.main.temp_min;
-  document.getElementById("max-temp").innerText = weatherData.main.temp_max;
-  document.getElementById("weather-output").classList.add('visible');
+function searchCity(){
+    let city = document.getElementById("input").value
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=celcius`)
+    .then(res => res.json())
+    .then(data => {
+        // console.log(data)
+        const city_name = data.name
+        const temprature = data.main.temp
+        const min_temp = data.main.temp_min
+        const max_temp = data.main.temp_max
+        const weather_type = data.weather[0].main
+
+        document.getElementById("city").innerText = city_name
+        document.getElementById("temp").innerText = temprature
+        document.getElementById("min-temp").innerText = min_temp
+        document.getElementById("max-temp").innerText = max_temp
+        document.getElementById("weather-type").innerText = weather_type
+    })
+    .catch(err => console.log(err))
 }
